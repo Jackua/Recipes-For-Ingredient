f85578d36a3309131805e700fc6b696d6039f235
Jackua <kuangja@oregonstate.edu>
2024-03-04 13:49:43 -0500

    Installed ZeroMQ and updated index.js to remove file extension from URL.

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.static("public"));
+app.use(express.static("public", { extensions: ["html"] }));
+
+app.get("/", (req, res) => {
+  res.sendFile();
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
